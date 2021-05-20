455bfda0698a2d2ed4a6aa408e126c5551d3872c
shinji0320 <shinji.blue.one@gmail.com>
2021-05-20 09:20:06 +0900

    [add]show

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        // $posts = Post::all();
+        $posts = Post::latest()->get();
+        // dd($posts->toArray());
+        return view('posts.index')->with('posts', $posts);
+    }
+
+    public function show($id) {
+        $post = Post::findOrFail($id);
+        return view('posts.show')->with('post', $post);
     }
 }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,14 +5,20 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Blog Posts</title>
+  <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
   <div class="container">
     <h1>Blog Posts</h1>
     <ul>
-      <li><a href="">title</a></li>
-      <li><a href="">title</a></li>
-      <li><a href="">title</a></li>
+    @foreach($posts as $post)
+      <li><a href="{{ url('/posts', $post->id )}}">{{ $post->title }}</a></li>
+    @endforeach
+    <!-- @forelse($posts as $post)
+      <li><a href="">{{ $post->title }}</a></li>
+    @empty
+      <li>No posts yet</li>
+    @endforelse -->
     </ul>
   </div>
 </body>
