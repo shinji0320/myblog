fcf78c7ed0e2d826006448c524ca05a0e3176091
shinji0320 <shinji.blue.one@gmail.com>
2021-05-20 17:02:18 +0900

    編集機能追加

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,30 +1,32 @@
 @extends('layouts.default')
 
-@section('title', 'New Post')
+@section('title', 'Edit Post')
 
 
 @section('content')
   <h1>
     <a href="{{ url('/') }}" class="header-menu">back</a>
-    New Post
+    Edit Post
   </h1>
-  <form method="post" action="{{ url('/posts') }}">
+  <form method="post" action="{{ url('/posts', $post->id) }}">
     {{ csrf_field() }}
+    {{ method_field('patch') }}
+
 
     <p>
-      <input type="text" name="title" placeholder="enter title" value="{{ old('titile') }}">
+      <input type="text" name="title" placeholder="enter title" value="{{ old('titile', $post->title) }}">
       @if ($errors->has('title'))
         <span class="error">{{ $errors->first('title') }}</span>
       @endif
     </p>
-      <textarea name="body" placeholder="enter body" value="{{ old('body' )}}"></textarea>
+      <textarea name="body" placeholder="enter body" value="{{ old('body', $post->body) }}"></textarea>
     <p>
       @if ($errors->has('body'))
         <span class="error">{{ $errors->first('body') }}</span>
       @endif
     </p>
     <p>
-      <input type="submit" value="Add">
+      <input type="submit" value="Update">
     </p>
   </form>
 @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,15 +7,24 @@ Blog Posts
 @section('content')
 <h1>
   <a href="{{ url('/posts/create') }}" class="header-menu">New Post</a>
-  Blog Posts
 </h1>
     <ul>
     @forelse($posts as $post)
-      <li><a href="{{ action('PostsController@show', $post)}}">{{ $post->title }}</a></li>
+      <li>
+        <a href="{{ action('PostsController@show', $post)}}">{{ $post->title }}</a>
+        <a href="{{ action('PostsController@edit', $post)}}" class="edit">[ Edit ]</a>
+        <a href="#" class="del" data-id="{{ $post->id }}">[ x ]</a>
+      </li>
+      <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
+        {{ csrf_field() }}
+        {{ method_filed('delete') }}
+
+    　</form>
     @empty
       <li>No posts yet</li>
     @endforelse
     </ul>
+    <script src="/js/main.js"></script>
 @endsection
 
 
